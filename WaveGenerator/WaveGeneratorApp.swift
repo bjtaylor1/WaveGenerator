5a4cfea02aba2c89975ce879fec5da98c85195b2
Ben Taylor <bentaylortheonly@gmail.com>
2026-03-06 14:04:41 +0000

    Initial Commit

---
+++ WaveGenerator/WaveGeneratorApp.swift
@@ -0,0 +1,17 @@
+//
+//  WaveGeneratorApp.swift
+//  WaveGenerator
+//
+//  Created by Ben Taylor on 06/03/2026.
+//
+
+import SwiftUI
+
+@main
+struct WaveGeneratorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
